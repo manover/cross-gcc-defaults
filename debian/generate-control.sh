a86e8504d898dd43a27e2722ba7f2dffa2e8ac2f
Daniel Gröber <dxld@darkboxed.org>
2014-03-02 11:58:04 +0100

    Add missing $arch suffix to source package name

--- debian/generate-control.sh
+++ debian/generate-control.sh
@@ -8,7 +8,9 @@ fi
 PROG_LIST=$(cat debian/defaults | awk '{ print $1 }')
 DEB_TARGET_GNU_TYPE=$(dpkg-architecture -a$DEB_TARGET_ARCH -qDEB_HOST_GNU_TYPE -f 2>/dev/null)
 
-(cat debian/control.head.in; echo) > debian/control
+cat debian/control.head.in \
+    | sed -e 's/$DEB_TARGET_ARCH/'"$DEB_TARGET_ARCH"'/g' > debian/control
+echo >> debian/control
 
 for prog in $PROG_LIST; do
     ver=$(cat debian/defaults | grep "^$prog" | awk '{ print $2 }')
